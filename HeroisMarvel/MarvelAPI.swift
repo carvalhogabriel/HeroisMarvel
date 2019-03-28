8c98b813470a353e0887ab9055050bbcc5ca1d30
Gabriel Guerrero <gabriel.guerrero@venturus.org.br>
2019-03-28 08:53:17 -0300

    App Done.

---
+++ HeroisMarvel/MarvelAPI.swift
@@ -0,0 +1,51 @@
+//
+//  MarvelAPI.swift
+//  HeroisMarvel
+//
+//  Created by Gabriel Carvalho Guerrero on 28/03/19.
+//  Copyright © 2019 Eric Brito. All rights reserved.
+//
+
+import Foundation
+import SwiftHash
+import Alamofire
+
+class MarvelAPI {
+    
+    static private let basePath = "https://gateway.marvel.com/v1/public/characters?"
+    static private let privateKey = "5a0aad32e33b17fbc3d8197f4b281f2b7318b9e5"
+    static private let publicKey = "09cf35a2700b066a780388724c29dcac"
+    static private let limit = 50
+    
+    private class func getCredentials() -> String {
+        let ts = String(Date().timeIntervalSince1970)
+        let hash = MD5(ts+privateKey+publicKey).lowercased()
+        return "ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
+    }
+    
+    class func loadHeroes(name: String?, page: Int = 0, onComplete: @escaping (MarvelInfo?) -> Void) {
+        let offset = page * limit
+        let startsWith: String
+        if let name = name, !name.isEmpty {
+            startsWith = "nameStartsWIth=\(name.replacingOccurrences(of: " ", with: ""))&"
+        } else {
+            startsWith = ""
+        }
+        
+        let url = basePath + "offset=\(offset)&limit=\(limit)&" + startsWith + getCredentials()
+        print(url)
+        
+        AF.request(url).responseJSON { (response) in
+            guard let data = response.data else {return}
+            
+            do {
+                let marvelInfo = try JSONDecoder().decode(MarvelInfo.self, from: data)
+                onComplete(marvelInfo)
+            } catch {
+                print(error.localizedDescription)
+                onComplete(nil)
+            }
+        }
+        
+    }
+}
